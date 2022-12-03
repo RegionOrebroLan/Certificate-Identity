b1f42ac7bed19ecefa2927efe932c3ab68719439
HansKindberg <hans.kindberg@gmail.com>
2022-12-03 10:12:28 +0100

    IdentityServer-client: Diagnostics-page showing more claims and refactored

--- Clients/IdentityServer/Pages/Diagnostics/Index.cshtml.cs
+++ Clients/IdentityServer/Pages/Diagnostics/Index.cshtml.cs
@@ -1,7 +1,13 @@
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using Duende.IdentityServer.Extensions;
+using Duende.IdentityServer.Test;
+using IdentityModel;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Authorization;
 
 namespace IdentityServer.Pages.Diagnostics;
 
@@ -9,18 +15,93 @@ namespace IdentityServer.Pages.Diagnostics;
 [Authorize]
 public class Index : PageModel
 {
-    public ViewModel View { get; set; }
-        
-    public async Task<IActionResult> OnGet()
-    {
-        var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
-        if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
-        {
-            return NotFound();
-        }
+	#region Fields
 
-        View = new ViewModel(await HttpContext.AuthenticateAsync());
-            
-        return Page();
-    }
+	private ViewModel _view;
+
+	#endregion
+
+	#region Constructors
+
+	public Index(TestUserStore userStore)
+	{
+		this.UserStore = userStore ?? throw new ArgumentNullException(nameof(userStore));
+	}
+
+	#endregion
+
+	#region Properties
+
+	protected internal virtual TestUserStore UserStore { get; }
+
+	public virtual ViewModel View
+	{
+		get
+		{
+			if(this._view == null)
+			{
+				var view = new ViewModel();
+
+				var authenticateResult = this.HttpContext.AuthenticateAsync().Result;
+
+				var claims = new List<Claim>();
+				claims.AddRange(authenticateResult.Principal.Claims);
+
+				var subject = authenticateResult.Principal.GetSubjectId();
+
+				var user = this.UserStore.FindBySubjectId(subject);
+
+				foreach(var claim in user.Claims)
+				{
+					if(claims.Any(item => string.Equals(item.Type, claim.Type, StringComparison.OrdinalIgnoreCase)))
+						continue;
+
+					claims.Add(claim);
+				}
+
+				claims.Sort((first, second) => string.Compare(first?.Type, second?.Type, StringComparison.OrdinalIgnoreCase));
+
+				foreach(var claim in claims)
+				{
+					view.Claims.Add(claim);
+				}
+
+				if(authenticateResult.Properties.Items.ContainsKey("client_list"))
+				{
+					var encoded = authenticateResult.Properties.Items["client_list"];
+					var bytes = Base64Url.Decode(encoded);
+					var value = Encoding.UTF8.GetString(bytes);
+
+					foreach(var client in JsonSerializer.Deserialize<string[]>(value))
+					{
+						view.Clients.Add(client);
+					}
+				}
+
+				foreach(var (key, value) in authenticateResult.Properties.Items)
+				{
+					view.Properties.Add(key, value);
+				}
+
+				this._view = view;
+			}
+
+			return this._view;
+		}
+	}
+
+	#endregion
+
+	#region Methods
+
+	public async Task<IActionResult> OnGet()
+	{
+		var localAddresses = new string[] { "127.0.0.1", "::1", this.HttpContext.Connection.LocalIpAddress.ToString() };
+		if(!localAddresses.Contains(this.HttpContext.Connection.RemoteIpAddress.ToString()))
+			return this.NotFound();
+
+		return await Task.FromResult(this.Page());
+	}
+
+	#endregion
 }
--- Clients/IdentityServer/Pages/Diagnostics/ViewModel.cs
+++ Clients/IdentityServer/Pages/Diagnostics/ViewModel.cs
@@ -1,30 +1,17 @@
 // Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
-
-using IdentityModel;
-using Microsoft.AspNetCore.Authentication;
-using System.Text;
-using System.Text.Json;
+using System.Security.Claims;
 
 namespace IdentityServer.Pages.Diagnostics;
 
 public class ViewModel
 {
-    public ViewModel(AuthenticateResult result)
-    {
-        AuthenticateResult = result;
+	#region Properties
 
-        if (result.Properties.Items.ContainsKey("client_list"))
-        {
-            var encoded = result.Properties.Items["client_list"];
-            var bytes = Base64Url.Decode(encoded);
-            var value = Encoding.UTF8.GetString(bytes);
+	public virtual IList<Claim> Claims { get; } = new List<Claim>();
+	public virtual IList<string> Clients { get; } = new List<string>();
+	public virtual IDictionary<string, string> Properties { get; } = new SortedDictionary<string, string>(StringComparer.OrdinalIgnoreCase);
 
-            Clients = JsonSerializer.Deserialize<string[]>(value);
-        }
-    }
-
-    public AuthenticateResult AuthenticateResult { get; }
-    public IEnumerable<string> Clients { get; } = new List<string>();
+	#endregion
 }
